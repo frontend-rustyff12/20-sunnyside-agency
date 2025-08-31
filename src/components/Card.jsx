06e38787e0566b405b1c5f28f19dd85a0f4a168a
Russell <75209707+rustyff12@users.noreply.github.com>
2025-08-31 12:22:05 +0200

    Testimonials layouts

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,19 @@
-export default function Card() {
+export default function Card({ item }) {
   return (
-    <>
-      <h2>Card goes here</h2>
-    </>
+    <article className="card">
+      <div className="card-top">
+        <img
+          className="card-image"
+          src={item.img}
+          alt={`Image of ${item.name}`}
+        />
+        <p className="card-description">{item.description}</p>
+      </div>
+
+      <div className="card-bottom">
+        <p className="name">{item.name}</p>
+        <p className="title">{item.title}</p>
+      </div>
+    </article>
   );
 }
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,7 +1,39 @@
+import Card from "./Card";
 export default function Testimonial() {
+  const carData = [
+    {
+      name: "Emily R.",
+      title: "Marketing Director",
+      description:
+        " We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.",
+      img: "/image-emily.jpg",
+    },
+    {
+      name: "Thomas S.",
+      title: "Chief Operating Officer",
+      description:
+        "Sunnyside&apos;s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.",
+      img: "/image-thomas.jpg",
+    },
+    {
+      name: "Jennie F.",
+      title: "Business Owner",
+      description:
+        "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!",
+      img: "/image-jennie.jpg",
+    },
+  ];
+
+  const TestimonialElements = carData.map((item, index) => (
+    <Card item={item} key={index} />
+  ));
   return (
     <>
-      <h2>Testimonial goes here</h2>
+      <section className="testimonials">
+        <h4>Client Testimonials</h4>
+        <div className="testimonials-wrapper">{TestimonialElements}</div>
+      </section>
+      )
     </>
   );
 }
