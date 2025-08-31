551eb480dc7f3c854ea4cecfcc5c822093751548
Russell <75209707+rustyff12@users.noreply.github.com>
2025-08-31 15:06:04 +0200

    Footer layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,51 @@
 export default function Footer() {
   return (
-    <>
-      <h2>Footer goes here</h2>
-    </>
+    <footer>
+      <div className="footer-content">
+        <img className="logo" src="/logo.svg" alt="Sunnyside company logo" />
+        <nav>
+          <ul className="footer-nav-links">
+            <li>
+              <a className="footer-link" href="#">
+                About
+              </a>
+            </li>
+            <li>
+              <a className="footer-link" href="#">
+                Services
+              </a>
+            </li>
+            <li>
+              <a className="footer-link" href="#">
+                Projects
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <ul className="footer-socials">
+          <li>
+            <a href="" target="_blank" rel="noopener noreferrer">
+              <img src="/icon-facebook.svg" alt="Link to facebook" />
+            </a>
+          </li>
+          <li>
+            <a href="" target="_blank" rel="noopener noreferrer">
+              <img src="/icon-instagram.svg" alt="Link to instagram" />
+            </a>
+          </li>
+          <li>
+            <a href="" target="_blank" rel="noopener noreferrer">
+              <img src="/icon-twitter.svg" alt="Link to twitter" />
+            </a>
+          </li>
+          <li>
+            <a href="" target="_blank" rel="noopener noreferrer">
+              <img src="/icon-pinterest.svg" alt="Link to pinterest" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
   );
 }
